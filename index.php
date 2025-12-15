c56da65db995823bfd9e1f0d3ac8dfa21c1a20b2
malagadesign <malaga.design@gmail.com>
2025-12-15 15:11:56 -0300

    Add root front controller and simplify .htaccess rewrite

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Front controller para hosting compartido.
+ *
+ * Este archivo simplemente carga el index.php original de Laravel
+ * que está dentro de la carpeta public/.
+ */
+
+require __DIR__ . '/public/index.php';
